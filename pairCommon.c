7ac1bee42f2a086c758d93a33b18b436f7450a80
srinath-93 <77610922+srinath-93@users.noreply.github.com>
2021-02-25 01:17:41 +0530

    Update pairCommon.c

--- pairCommon.c
+++ pairCommon.c
@@ -1,6 +1,13 @@
 #include "main_prv.h"
 #include "main_declare.h"
 
+const char* MajorColorNames[] = {"White", "Red", "Black", "Yellow", "Violet"};
+const char* MinorColorNames[] = {"Blue", "Orange", "Green", "Brown", "Slate"};
+const int MAX_COLORPAIR_NAME_CHARS = 16;
+int numberOfMajorColors = sizeof(MajorColorNames) / sizeof(MajorColorNames[0]);
+int numberOfMinorColors = sizeof(MinorColorNames) / sizeof(MinorColorNames[0]);
+
+
 void ColorPairToString(const ColorPair* colorPair, char* buffer)
 {
     sprintf(buffer, "%s %s",
