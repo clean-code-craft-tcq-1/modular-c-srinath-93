eac340ddf4e615caf5ddc5bf9e4fe120b5bbd23a
srinath-93 <77610922+srinath-93@users.noreply.github.com>
2021-02-25 02:52:52 +0530

    Update main.c

--- main.c
+++ main.c
@@ -10,17 +10,18 @@
  * param[inout]         : None
  * param[out]           : None
  * return               : int
- * Description          : Program to verify color number with color's pair
- *                       and to verify color's pair with corresponding color's number
+ * Description          : Program to verify color number with color's pair and to verify color's pair with
+ *                        corresponding color's number and print refernce manual
  **********************************************************************************************************************
  */
 int main() {
+    /* test if color number is inline with color pair */
     verifyNumberToPair(4, WHITE, BROWN);
     verifyNumberToPair(5, WHITE, SLATE);
-
+    /* test if color pair is inline with color number */
     verifyPairToNumber(BLACK, ORANGE, 12);
     verifyPairToNumber(VIOLET, SLATE, 25);
-    
+    /* print the reference manual */
     printRefernceManual();
 
     return 0;
