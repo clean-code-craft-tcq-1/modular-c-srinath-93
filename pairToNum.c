a2a74ee58d4da417505847519323fd72809caaa5
srinath-93 <77610922+srinath-93@users.noreply.github.com>
2021-02-25 02:11:06 +0530

    Update pairToNum.c

--- pairToNum.c
+++ pairToNum.c
@@ -1,6 +1,17 @@
 #include "main_prv.h"
 #include "main_declare.h"
 
+/*
+ **********************************************************************************************************************
+ * Service name         : verifyPairToNumber
+ * Syntax               : void verifyPairToNumber(enum MajorColor major, enum MinorColor minor, int expectedPairNumber)
+ * param[in]            : major
+ * param[in]            : minor
+ * param[in]            : expectedPairNumber
+ * return               : None
+ * Description          : Program to verify color's pair with color number
+ **********************************************************************************************************************
+ */
 void verifyPairToNumber(enum MajorColor major, enum MinorColor minor, int expectedPairNumber)
 {
     ColorPair colorPair;
