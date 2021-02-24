f9ad769f3aa034b6dc83d10c38b218298edadef3
srinath-93 <77610922+srinath-93@users.noreply.github.com>
2021-02-25 02:05:24 +0530

    Update main.c

--- main.c
+++ main.c
@@ -2,13 +2,24 @@
 #include <assert.h>
 #include "main_prv.h"
 #include "main_declare.h"
-
+/*
+ **********************************************************************************************************************
+ * Service name         : main
+ * Syntax               : int main(void)
+ * param[in]            : None
+ * param[inout]         : None
+ * param[out]           : None
+ * return               : int
+ * Description          : Program to verify color number with color's pair
+ *                       and to verify color's pair with corresponding color's number
+ **********************************************************************************************************************
+ */
 int main() {
-    testNumberToPair(4, WHITE, BROWN);
-    testNumberToPair(5, WHITE, SLATE);
+    verifyNumberToPair(4, WHITE, BROWN);
+    verifyNumberToPair(5, WHITE, SLATE);
 
-    testPairToNumber(BLACK, ORANGE, 12);
-    testPairToNumber(VIOLET, SLATE, 25);
+    verifyPairToNumber(BLACK, ORANGE, 12);
+    verifyPairToNumber(VIOLET, SLATE, 25);
 
     return 0;
 }
