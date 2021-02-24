9e8f7ea3edc48e95c024f7a8b75638105b2a34e5
srinath-93 <77610922+srinath-93@users.noreply.github.com>
2021-02-25 00:45:05 +0530

    Update main_declare.h

--- main_declare.h
+++ main_declare.h
@@ -1,3 +1,8 @@
+#ifndef MAIN_DECLARE_H_
+#define MAIN_DECLARE_H_
+
+#include <stdio.h>
+#include <assert.h>
 #include "main_prv.h"
 
 extern void ColorPairToString(const ColorPair* colorPair, char* buffer);
@@ -9,3 +14,5 @@ extern int GetPairNumberFromColor(const ColorPair* colorPair);
 extern void testNumberToPair(int pairNumber, enum MajorColor expectedMajor, enum MinorColor expectedMinor);
 
 extern void testPairToNumber(enum MajorColor major, enum MinorColor minor, int expectedPairNumber);
+
+#endif /* MAIN_DECLARE_H_ */
