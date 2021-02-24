7059ae4c06aedf0c7cdfc632958ef5105545fc55
srinath-93 <77610922+srinath-93@users.noreply.github.com>
2021-02-25 02:45:12 +0530

    Update main_prv.h

--- main_prv.h
+++ main_prv.h
@@ -1,6 +1,10 @@
 #ifndef MAIN_PRV_H_
 #define MAIN_PRV_H_
-
+/*
+ **********************************************************************************************************************
+ * Type definitions (simple: enums, return-types)
+ **********************************************************************************************************************
+ */
 enum MajorColor {WHITE, RED, BLACK, YELLOW, VIOLET};
 enum MinorColor {BLUE, ORANGE, GREEN, BROWN, SLATE};
 
