afb02be9e32e003235754d112939f2ac8e0e3f0d
srinath-93 <77610922+srinath-93@users.noreply.github.com>
2021-02-25 01:49:32 +0530

    Update numToPair.c

--- numToPair.c
+++ numToPair.c
@@ -9,6 +9,7 @@ void testNumberToPair(int pairNumber, enum MajorColor expectedMajor, enum MinorC
     char colorPairNames[MAX_COLORPAIR_NAME_CHARS];
     ColorPairToString(&colorPair, colorPairNames);
     printf("Got pair %s\n", colorPairNames);
+    printf("Major colour - %s\n Minor colour - %s\n",colorPair.majorColor,colorPair.minorColor);
     assert(colorPair.majorColor == expectedMajor);
     assert(colorPair.minorColor == expectedMinor);
 }
