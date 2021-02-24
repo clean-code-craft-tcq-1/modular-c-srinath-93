7049797e869441244abe8aefa5fd05cf4f1def7a
srinath-93 <77610922+srinath-93@users.noreply.github.com>
2021-02-25 02:05:27 +0530

    Update main_declare.h

--- main_declare.h
+++ main_declare.h
@@ -15,8 +15,8 @@ extern ColorPair GetColorFromPairNumber(int pairNumber);
 
 extern int GetPairNumberFromColor(const ColorPair* colorPair);
 
-extern void testNumberToPair(int pairNumber, enum MajorColor expectedMajor, enum MinorColor expectedMinor);
+extern void verifyNumberToPair(int pairNumber, enum MajorColor expectedMajor, enum MinorColor expectedMinor);
 
-extern void testPairToNumber(enum MajorColor major, enum MinorColor minor, int expectedPairNumber);
+extern void verifyPairToNumber(enum MajorColor major, enum MinorColor minor, int expectedPairNumber);
 
 #endif /* MAIN_DECLARE_H_ */
