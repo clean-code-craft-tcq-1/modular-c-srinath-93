1e37eb9b60c31d6ed916d8c6d0378a91b35b8e22
srinath-93 <77610922+srinath-93@users.noreply.github.com>
2021-02-25 02:09:41 +0530

    Update numToPair.c

--- numToPair.c
+++ numToPair.c
@@ -2,7 +2,17 @@
 #include "main_declare.h"
 
 extern const int MAX_COLORPAIR_NAME_CHARS;
-
+/*
+ **********************************************************************************************************************
+ * Service name         : verifyNumberToPair
+ * Syntax               : void verifyNumberToPair(int pairNumber, enum MajorColor expectedMajor, enum MinorColor expectedMinor)
+ * param[in]            : pairNumber
+ * param[in]            : MajorColor
+ * param[in]            : expectedMinor
+ * return               : None
+ * Description          : Program to verify color number with color's pair
+ **********************************************************************************************************************
+ */
 void verifyNumberToPair(int pairNumber, enum MajorColor expectedMajor, enum MinorColor expectedMinor)
 {
     ColorPair colorPair = GetColorFromPairNumber(pairNumber);
